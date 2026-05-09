7d223288890579febbdb91d2435d0fc5df335bbd
pujarigovinda1999-hash <pujarigovinda1999@gmail.com>
2026-05-09 09:18:52 +0530

    Update init.database.sql

--- scripts/init.database.sql
+++ scripts/init.database.sql
@@ -14,11 +14,7 @@ WARNING:
 */
 
 -- Drop and recreate the 'DataWarehouse' database
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
-BEGIN
-    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-    DROP DATABASE DataWarehouse;
-END;
+
 
 -- Create the 'DataWarehouse' database
 CREATE DATABASE DataWarehouse;
